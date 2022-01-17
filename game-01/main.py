d3ec6b5e45e755673928a1ae44e3355fca09e1e7
Gonzalo Marin <gonzalo@MacBook-Pro-de-Gonzalo.local>
2022-01-17 14:31:42 -0300

    Sort array and Limit Search

--- game-01/main.py
+++ game-01/main.py
@@ -2,10 +2,11 @@ from input_array import input_array
 def main():
     array = input_array()
     suma = int(input("Ingresa el resultado de la suma : "))
+    array.sort()
     position_sumando_A=0
-    while position_sumando_A < len(array):
+    while array[position_sumando_A] < suma:
         position_sumando_B = position_sumando_A+1
-        while position_sumando_B < len(array):
+        while array[position_sumando_B] < suma:
             if (array[position_sumando_A] + array[position_sumando_B]) == suma:
                 print("El primer subconjunto de 2 numeros que de resultado", suma, "son el", array[position_sumando_A], "y", array[position_sumando_B])
                 exit()
